14236a26564684402f770c0ffb3ba113c510a106
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-07 18:32:41 -1000

    Add files via upload

---
+++ Gods_Eye_7L.cmd
@@ -0,0 +1,34 @@
+cd /
+:top
+cls
+mod con: lines=6 cols=30
+title "Gods_Eye_7L"
+@cecho off 
+          
+@set "apk=952718647123"
+@set "app=978541321643"
+@set "exe=981721365432"
+cecho {04}
+
+set /a m=Choose;
+if (%m%=1) & goto one
+if (%m%=2) & goto two
+if (%m%=3) & goto three
+goto top
+
+:one
+copy "%apk% ./god_eye.apk"
+goto 7L
+
+:two
+copy "%app% ./god_eye.app"
+goto 7L
+
+:three
+copy "%exe% ./god_eye.exe"
+goto 7L
+
+:7L
+cecho {05}Made_By_Kia7L{02}
+@timeout /t 5
+goto top
