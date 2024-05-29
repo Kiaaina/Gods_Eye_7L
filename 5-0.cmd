5f5a0d60155c3489ab7c40be97020dca3f98fb0a
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-29 01:12:33 -1000

    Update 5-0.cmd

--- 5-0.cmd
+++ 5-0.cmd
@@ -1,7 +1,7 @@
 cd /
 :top
 cls
-mode con: lines=6 cols=30
+mode 20,5
 title "5-0_7L"
 @echo off 
 @set "cecho=.\cecho.exe"          
@@ -9,7 +9,7 @@ title "5-0_7L"
 @set "medic=111111111111"
 @set "police=222222222222"
 
-%cecho% {04}1=fire{\n}2=medic{\n}3=police{07}
+%cecho% {04}1=fire{\n}2=medic{\n}3=police{07}{\n}
 
 set /p m=Choose;
 if (%m%=1) goto one
@@ -18,18 +18,18 @@ if (%m%=3) goto three
 goto top
 
 :one
-copy "%fire% ./fire.exe"
+copy "%fire%" "./fire.exe"
 goto 7L
 
 :two
-copy "%medic% ./medic.exe"
+copy "%medic%" "./medic.exe"
 goto 7L
 
 :three
-copy "%police% ./police.exe"
+copy "%police%" "./police.exe"
 goto 7L
 
 :7L
 %cecho% {05}Made_By_Kia7L{02}
-@timeout /t 5
+@timeout /t 5 > nul
 goto top
